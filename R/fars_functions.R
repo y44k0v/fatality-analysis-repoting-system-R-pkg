ea1c9f50fd6747400fd93564080c93ee49c2aaf3
ykv001 <ykv00001@gmail.com>
2017-04-28 12:05:24 -0400

    Cahnged some NSE to SE

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -88,8 +88,8 @@ fars_read_years <- function(years) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>%
-                                dplyr::select(MONTH, year)
+                        dplyr::mutate_(dat, year = year) %>%
+                                dplyr::select_(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
                         return(NULL)
@@ -123,9 +123,9 @@ fars_read_years <- function(years) {
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>%
-                dplyr::group_by(year, MONTH) %>%
-                dplyr::summarize(n = n()) %>%
-                tidyr::spread(year, n)
+                dplyr::group_by_(year, MONTH) %>%
+                dplyr::summarize_(n = n()) %>%
+                tidyr::spread_(year, n)
 }
 
 #' Plotting fatalities' state locations by  year
@@ -162,7 +162,7 @@ fars_map_state <- function(state.num, year) {
 
         if(!(state.num %in% unique(data$STATE)))
                 stop("invalid STATE number: ", state.num)
-        data.sub <- dplyr::filter(data, STATE == state.num)
+        data.sub <- dplyr::filter_(data, STATE == state.num)
         if(nrow(data.sub) == 0L) {
                 message("no accidents to plot")
                 return(invisible(NULL))
